d4f94cb783f4b6ab38c2e50dbc1e7b03d697c0ec
Debatreya <debatreyadas@gmail.com>
2024-12-31 21:32:19 +0530

    Done till Chapter 15

---
+++ app/lib/actions.ts
@@ -0,0 +1,124 @@
+"use server";
+
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+	id: z.string(),
+	customerId: z.string({
+		invalid_type_error: "Please select a customer.",
+	}),
+	amount: z.coerce
+		.number()
+		.gt(0, { message: "Please enter an amount greater than $0." }),
+	status: z.enum(["pending", "paid"], {
+		invalid_type_error: "Please select an invoice status.",
+	}),
+	date: z.string(),
+});
+
+export type State = {
+	errors?: {
+		customerId?: string[];
+		amount?: string[];
+		status?: string[];
+	};
+	message?: string | null;
+};
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+// CreateInvoice: { customerId: string; amount: number; status: "pending" | "paid" }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(prevState: State, formData: FormData) {
+	// Validate form fields using Zod
+	const validatedFields = CreateInvoice.safeParse({
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	});
+
+	// Why zod is used here?
+	// Zod is used to validate the form data before inserting it into the database.
+	// This ensures that the data is in the correct format and that it is safe to use in the database.
+	// If the data is not in the correct format,
+	// an error will be thrown and the data will not be inserted into the database.
+
+	// If form validation fails, return errors early. Otherwise, continue.
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing Fields. Failed to Create Invoice.",
+		};
+	}
+
+	// Prepare data for insertion into the database
+	const { customerId, amount, status } = validatedFields.data;
+
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split("T")[0];
+
+	try {
+		await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+	} catch (error) {
+		console.error(error);
+		return {
+			message: "Database Error: Failed to Create Invoice.",
+		};
+	}
+	revalidatePath("/dashboard/invoices");
+	redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(
+	id: string,
+	prevState: State,
+	formData: FormData
+) {
+	const validatedFields = UpdateInvoice.safeParse({
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	});
+
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing Fields. Failed to Update Invoice.",
+		};
+	}
+
+	const { customerId, amount, status } = validatedFields.data;
+	const amountInCents = amount * 100;
+
+	try {
+		await sql`
+		UPDATE invoices
+		SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+		WHERE id = ${id}
+	  `;
+	} catch (error) {
+		console.error(error);
+		return { message: "Database Error: Failed to Update Invoice." };
+	}
+
+	revalidatePath("/dashboard/invoices");
+	redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+	try {
+		await sql`DELETE FROM invoices WHERE id = ${id}`;
+		revalidatePath("/dashboard/invoices");
+		// return { message: "Deleted Invoice." }; --> This will create problem because this function is used like <form action={deleteInvoiceWithId}> ... </form> which expects a function to return a path to redirect to.
+	} catch (error) {
+		// return { message: "Database Error: Failed to Delete Invoice." };
+		console.error(error);
+	}
+}
